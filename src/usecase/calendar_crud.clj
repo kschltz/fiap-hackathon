1cdebb6f8f2690ea6bbbd04b8b53bde9493bdf68
Caio Santos <caio.css@outlook.com>
2024-07-25 15:15:26 -0300

    add schedule endpoint

--- src/usecase/calendar_crud.clj
+++ src/usecase/calendar_crud.clj
@@ -20,6 +20,23 @@
     (tap> [::20 to-insert])
     (create-calendar node to-insert)))
 
+(defn slot-available? [availabilities time]
+  (some #(and (= (:from %) time) (not (:booked %))) availabilities))
+
+(defn get-calendar [node medic-id date]
+  (xt/entity (xt/db node) (str date "#" medic-id)))
+
+(defn book-appointment [node medic-id date time patient-id]
+  (let [calendar (get-calendar node medic-id date)
+        updated-availabilities (map (fn [slot]
+                                      (if (and (= (:from slot) time) (not (:booked slot)))
+                                        (assoc slot :booked true :patient-id patient-id)
+                                        slot))
+                                    (:availabilities calendar))
+        updated-calendar (assoc calendar :availabilities updated-availabilities)]
+    (update-calendar node updated-calendar)))
+
+
 (comment
   (malli.generator/sample calendar/Calendar)
 
@@ -40,4 +57,21 @@
                           :to   (LocalTime/of 19 0)}))))
 
   (xt/q (user/db) '{:find  [(pull e [*])]
-                    :where [[e :xt/type :calendar]]}))
+                    :where [[e :xt/type :calendar]]})
+
+  (def medic-calendar-example
+    {:medic-id       #uuid"01582fa7-cd5a-4f0a-be8c-9b776a6ca3d6"
+     :year           2024
+     :month          9
+     :day            19
+     :availabilities [{:from "08:00" :to "09:00" :booked? false}
+                      {:from "09:00" :to "10:00" :booked? false}
+                      {:from "10:00" :to "11:00" :booked? true :patient-id #uuid"01582fa7-cd5a-4f0a-be8c-9b776a6ca3d6"}
+                      {:from "12:00" :to "13:00" :booked? false}]})
+
+  (def request-appointment-example
+    {:medic-id #uuid"01582fa7-cd5a-4f0a-be8c-9b776a6ca3d6"
+     :date     "2024-09-19"
+     :time     "11:00"})
+
+  )
