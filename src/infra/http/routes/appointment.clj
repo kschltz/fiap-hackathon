073c77c367fe4f2bbba72961e261168657a1be8d
Caio Santos <caio.css@outlook.com>
2024-07-26 23:41:07 -0300

    fix appointment endpoint

--- src/infra/http/routes/appointment.clj
+++ src/infra/http/routes/appointment.clj
@@ -1,5 +1,6 @@
 (ns infra.http.routes.appointment
-  (:require [model.base :as base]
+  (:require [infra.http.server :as server]
+            [model.base :as base]
             [model.appointment :as appointment]
             [usecase.appointment :as uc.appointment]))
 
@@ -32,22 +33,8 @@
        :headers {"Content-Type" "application/json"}
        :body (.getMessage e)})))
 
-(defn manage-appointment [{:keys [app json-params]}]
-  (try
-    (let [{:keys [xtdb]} app
-          {:keys [appointment-id action]} (base/json-> appointment/ManageAppointment json-params)]
-      (case action
-        "accept" (uc.appointment/accept-appointment xtdb appointment-id)
-        "reject" (uc.appointment/reject-appointment xtdb appointment-id))
-      {:status 200 :body {:message "Appointment managed successfully"}})
-    (catch Exception e
-      {:status 500
-       :headers {"Content-Type" "application/json"}
-       :body (.getMessage e)})))
-
 (def routes
-  ["/patient/appointment"
-   {:post `schedule-appointment
-    :put  `delete-appointment}
-   "/medic/manage-appointment"
-   {:put `manage-appointment}])
+  ["/patient" ^:interceptors [server/authenticate-interceptor]
+   ["/appointment" ^:interceptors [(server/type-exclusive-interceptor "paciente")]
+    {:post `schedule-appointment
+     :put  `delete-appointment}]])
